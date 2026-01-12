688ab8ecaf4ba570a89b070ce37da8381159f6b3
Gaurav Singh Chauhan <gauravsinghchauhan60@gmail.com>
2026-01-12 14:03:05 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import cors from 'cors';
 import 'dotenv/config'; 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(cors());
 app.use(express.json());
